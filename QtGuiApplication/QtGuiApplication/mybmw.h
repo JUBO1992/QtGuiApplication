47dffbafac5c7105610c105aae910ad2029d87b1
JUBO <jubo_1992@163.com>
2019-04-22 23:29:13 +0800

    补充提交文件

---
+++ QtGuiApplication/QtGuiApplication/mybmw.h
@@ -0,0 +1,39 @@
+﻿#ifndef MYBMW_H
+#define MYBMW_H
+
+#include <QWidget>
+#include <QtGui>
+
+class MyBMW : public QWidget
+{
+	Q_OBJECT
+
+public:
+	MyBMW(QWidget *parent);
+	~MyBMW();
+protected:
+	void paintEvent(QPaintEvent *);
+
+	//重写sizeHint()
+	QSize sizeHint() const
+	{
+		return QSize(300, 300);
+	}
+
+private:
+	void drawUnderCircle(QPainter* painter);//画外圆
+
+	void drawBMW(QPainter* painter);//画宝马
+
+private:
+	QTimer* m_updateTimer;//定时器时间
+
+	qreal   m_angle;    //旋转角度
+	qreal   m_outerRadius;//外半径
+
+	private slots:
+	void UpdateAngle();//自定义槽，更新角度旋转
+	
+};
+
+#endif // MYBMW_H
---
+++ QtGuiApplication/QtGuiApplication/progressbase.h
@@ -0,0 +1,39 @@
+﻿#ifndef PROGRESSBASE_H
+#define PROGRESSBASE_H
+
+#include <QString>
+
+class ProgressBase
+{
+public:
+	ProgressBase(){}
+	virtual ~ProgressBase(){}
+
+	virtual void CreateProgress(const QString& info = QString()) = 0;
+	virtual void CloseProgress() = 0;
+	virtual void SetProgressValue(const int& curStep, const int& totalStep) = 0;
+	virtual void SetProgressText(const QString& info = QString()) = 0;
+
+	//1-创建；2-信息；3-关闭
+	virtual void AutoProgress(const int& mode, const QString& info = QString()) = 0;
+};
+
+class ProgressTracker :public ProgressBase
+{
+public:
+	ProgressTracker(const QString& info = QString());
+	virtual ~ProgressTracker();
+
+	virtual void CreateProgress(const QString& info = QString()) override;
+	virtual void CloseProgress() override;
+	virtual void SetProgressValue(const int& curStep, const int& totalStep) override;
+	virtual void SetProgressText(const QString& info = QString()) override;
+
+	//1-创建；2-信息；3-关闭
+	virtual void AutoProgress(const int& mode, const QString& info = QString()) override;
+
+private:
+	QString _info;
+};
+
+#endif // PROGRESSBASE_H
